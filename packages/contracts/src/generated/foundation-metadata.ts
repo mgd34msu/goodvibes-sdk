161a5cc4dae6d9dc65df38392df48bf2f1b1588f
Mike Davis <mgd34msu@gmail.com>
2026-07-27 15:22:25 -0500

    chore: release 1.18.0

--- packages/contracts/src/generated/foundation-metadata.ts
+++ packages/contracts/src/generated/foundation-metadata.ts
@@ -1,6 +1,6 @@
 export const FOUNDATION_METADATA = {
   "productId": "goodvibes",
-  "productVersion": "1.17.2",
+  "productVersion": "1.18.0",
   "operatorMethodCount": 443,
   "operatorEventCount": 32,
   "peerEndpointCount": 6
